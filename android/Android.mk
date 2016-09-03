b93be6c906082f708106df44547d1a4bac300b75
loki666 <simons.philippe@gmail.com>
2016-09-03 23:30:01 +0200

    android update shared make file

--- android/Android.mk
+++ android/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := fluidsynth_shared
 LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libfluidsynth.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/jni $(LOCAL_PATH)/../include
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../include $(LOCAL_PATH)/jni/include
 TARGET_PLATFORM = android-10
 
 include $(PREBUILT_SHARED_LIBRARY)
